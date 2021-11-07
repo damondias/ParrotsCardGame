e501a07717d487e1bcd90d704f31b93ef4cdda58
Damon Dias <damondias@gmail.com>
2021-11-07 00:45:47 -0400

    Requisito: Distribuir cartas viradas para baixo aleatoriamente.

--- scripts.js
+++ scripts.js
@@ -1,16 +1,46 @@
-let qtdDeCartas = 0;
-function inicio(){
-    qtdDeCartas = parseInt(prompt("Com quantas cartas quer jogar? "));
-    console.log(qtdDeCartas);
+let tipoDeCartas = ["assets/bobrossparrot.gif", "assets/explodyparrot.gif", 
+                    "assets/fiestaparrot.gif","assets/metalparrot.gif",
+                    "assets/revertitparrot.gif", "assets/tripletsparrot.gif", "assets/unicornparrot.gif"];
 
-    let regra = qtdDeCartas%2 !== 0 || qtdDeCartas < 4 || qtdDeCartas > 14; // Regra do jogo pelo requisito
-    console.log(regra);
+function inicio(){
+    let qtdDeCartas = parseInt(prompt("Com quantas cartas quer jogar? "));    
+
+    let regra = qtdDeCartas%2 !== 0 || qtdDeCartas < 4 || qtdDeCartas > 14; // Regra do jogo pelo requisito    
     while(regra){
-        qtdDeCartas = parseInt(prompt("Número inválido \nCom quantas cartas quer jogar?  ")); 
-        console.log(qtdDeCartas);
-        regra = qtdDeCartas%2 != 0 || qtdDeCartas < 4 || qtdDeCartas > 14;
-        console.log(regra);
-    }   
+        qtdDeCartas = parseInt(prompt("Número inválido \nCom quantas cartas quer jogar?  "));        
+        regra = qtdDeCartas%2 != 0 || qtdDeCartas < 4 || qtdDeCartas > 14;        
+    }
+    sortearCartas(qtdDeCartas);   
+}
+
+function sortearCartas(qtdDeCartas){
+    let qtdTipos = qtdDeCartas/2;    
+    let tipoDeCartas_sorteio = tipoDeCartas.sort(comparador);   
+    
+    let cartas =[];
+    for(let i=0; i<qtdTipos;i++){
+        for(j=0; j<2;j++){          
+           cartas.push(`${tipoDeCartas_sorteio[i]}`);
+        }
+    }    
+    console.log(cartas);    
+    let cartas_sorteadas = cartas.sort(comparador);    
+    
+    let cartas_jogo= document.querySelector("main");
+    for(i=0;i<cartas_sorteadas.length;i++){
+        cartas_jogo.innerHTML += `<div class="carta" data-identifier="card">
+            <div class="face frente" data-identifier="front-face">
+                <img src= ${cartas_sorteadas[i]} alt="parrot${i}">  
+            </div>
+            <div class="face verso" data-identifier="back-face">
+                <img src="assets/front.png" alt="parrot">
+            </div>
+          </div>`   
+    }
+
+}
+function comparador() { 
+	return Math.random() - 0.5; 
 }
 
 inicio();
