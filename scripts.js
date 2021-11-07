926b886eff75b65225576bea025ebb019bd8986a
Damon Dias <damondias@gmail.com>
2021-11-06 20:44:44 -0400

    O usuário só poderá inserir números pares no prompt, de 4 a 14. Qualquer número que fuja a essa regra não deve ser aceito. No caso de números inválidos, o prompt deverá ficar sendo repetido, até que o usuário coloque um número válido.

--- scripts.js
+++ scripts.js
@@ -1,2 +1,16 @@
-let qtdDeCartas = prompt("Com quantas cartas quer jogar? ");
-console.log(qtdDeCartas);
+let qtdDeCartas = 0;
+function inicio(){
+    qtdDeCartas = parseInt(prompt("Com quantas cartas quer jogar? "));
+    console.log(qtdDeCartas);
+
+    let regra = qtdDeCartas%2 !== 0 || qtdDeCartas < 4 || qtdDeCartas > 14; // Regra do jogo pelo requisito
+    console.log(regra);
+    while(regra){
+        qtdDeCartas = parseInt(prompt("Número inválido \nCom quantas cartas quer jogar?  ")); 
+        console.log(qtdDeCartas);
+        regra = qtdDeCartas%2 != 0 || qtdDeCartas < 4 || qtdDeCartas > 14;
+        console.log(regra);
+    }   
+}
+
+inicio();
